5898a47ac741bfbdc2c4bba85491c4f23edcc842
34j <55338215+34j@users.noreply.github.com>
2022-09-24 02:03:19 +0900

    Add docs.

--- src/pythonTerminal/types.ts
+++ src/pythonTerminal/types.ts
@@ -3,5 +3,10 @@
  * Provides a properly configured terminal for executing Python commands.
  */
 export interface IPythonTerminal {
+    /**
+     * Send a command to the terminal.
+     * @param options The options to send. (Must not include 'python' or the interpreter path.)
+     * @param addNewLine Whether to add a new line to the end of the command. This option is only used for the VSCode terminal.
+     */
     send(options: string[], addNewLine?: boolean): Promise<void>;
 }
