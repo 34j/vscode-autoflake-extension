59785420bd90f65e1de99124e52382f1cc363bc7
34j <55338215+34j@users.noreply.github.com>
2022-10-15 23:26:23 +0900

    bugfix: Add --recursive (works even if only 1 file

--- src/extension.ts
+++ src/extension.ts
@@ -33,7 +33,7 @@ export function activate(context: vscode.ExtensionContext) {
       ],
       ["jobs"],
       ["exclude", "imports"],
-      ["-i"]
+      ["--in-place", "--recursive"]
     )
   );
   disp.activate();
